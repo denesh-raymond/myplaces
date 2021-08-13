8494719b6d3a7fb993724f36df38ad06812956ca
Denesh Raymond <denesh.raymond@gmail.com>
2021-08-12 22:52:37 -0400

    Add Firebase Core and Authentication to sign in users anonymously

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myplaces/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+  // TODO - Store this user id in memory, so that we have a unique identifier for our db
+  // TODO - Handle authentication errors
+  String? userId = userCredential.user?.uid;
 
   runApp(App());
 }
